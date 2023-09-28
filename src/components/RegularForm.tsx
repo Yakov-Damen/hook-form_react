48f9971c831dc8a226c2eb49ee4515976d58b07f
Yakov Damen <yakov.damen@gmail.com>
2023-09-28 10:26:56 +0300

    Refctored using react hook form

--- src/components/RegularForm.tsx
+++ src/components/RegularForm.tsx
@@ -1,63 +1,82 @@
-import  { useState, ChangeEvent, FormEvent } from 'react';
-
+// import { useState, ChangeEvent, FormEvent } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 interface FormData {
   username: string;
   email: string;
-  password:string;
+  password: string;
 }
 
 function RegularForm() {
-  const [formData, setFormData] = useState<FormData>({
-    username: '',
-    email: '',
-    password: '',
-  });
+  // const [formData, setFormData] = useState<FormData>({
+  //   username: "",
+  //   email: "",
+  //   password: "",
+  // });
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+  const { register, handleSubmit, formState:{errors} } = useForm<FormData>();
+  const onSubmit: SubmitHandler<FormData> = (data) =>
+    alert(JSON.stringify(data));
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    alert(JSON.stringify(formData));
-  };
+  // const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   const { name, value } = e.target;
+  //   setFormData({
+  //     ...formData,
+  //     [name]: value,
+  //   });
+  // };
+
+  // const handleSubmi = (e: FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   alert(JSON.stringify(formData));
+  // };
 
   return (
-    <form onSubmit={handleSubmit}>
-        <h1>Change Me To React Hook Form</h1>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Change Me To React Hook Form</h1>
       <div>
         <input
+          {...register("username", { required: true, minLength: 2 })}
           type="text"
           id="username"
           name="username"
-          placeholder='Enter UserName'
-          value={formData.username}
-          onChange={handleChange}
+          placeholder="Enter UserName"
+          // value={formData.username}
+          // onChange={handleChange}
         />
+        <p>{errors.username?.message}</p>
       </div>
       <div>
         <input
+          {...register("email", {
+            required: true,
+            pattern: /^[a-zA-Z0-9-.]+@[a-zA-Z0-9-.]+(?:\[a-zA-Z0-9]+)*$/,
+          })}
           type="text"
           id="email"
           name="email"
-          placeholder='Enter Email'
-          value={formData.email}
-          onChange={handleChange}
+          placeholder="Enter Email"
+          // value={formData.email}
+          // onChange={handleChange}
         />
+        <p>{errors.email?.message}</p>
       </div>
       <div>
         <input
+          {...register("password", {
+            required: true,
+            minLength: 8,
+            maxLength: 20,
+            pattern:
+              /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$%^&*()_+-])[0-9A-Za-z@#$%^&*()_+-]/,
+          })}
           type="text"
           id="password"
           name="password"
-          placeholder='Enter Password'
-          value={formData.password}
-          onChange={handleChange}
+          placeholder="Enter Password"
+          // value={formData.password}
+          // onChange={handleChange}
         />
+        <p>{errors.password?.message}</p>
       </div>
       <button type="submit">Submit</button>
     </form>
